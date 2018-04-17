d3fe13d903fad3ceab132c1a65746bf616948a5b
bjitpatra1305 <biswajit1305@gmail.com>
2018-04-17 19:40:53 +0530

    Added socket io to chcek conncetion betweeen user and server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connceeted');
+
+  socket.on('disconnect', () => {
+    console.log('Connection got Disconnected');
+  })
+});
+
+server.listen(port, () => {
   console.log(`Server is up and ruiing on port ${port}`);
 });
