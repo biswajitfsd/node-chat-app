68549c46df9e2a98c2d02592615acd755c0e05ab
bjitpatra1305 <biswajit1305@gmail.com>
2018-04-18 18:00:19 +0530

    Now new user gets a welcome message from admin where as others get to know new user is joined

--- server/server.js
+++ server/server.js
@@ -12,7 +12,16 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connceeted');
-
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to chat user',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined chat box',
+    createdAt: new Date().getTime()
+  });
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -20,6 +29,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
